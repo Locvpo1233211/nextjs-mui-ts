f4c296341d402854ca75b5905845ce2ee4815672
Locvpo1233211 <trongnghia1112223333@gmail.com>
2025-04-02 17:23:51 +0700

    design footer

---
+++ src/components/footer/app.footer.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { AppBar, Container, IconButton, styled, Toolbar } from "@mui/material";
+import Fab from "@mui/material/Fab";
+import MenuIcon from "@mui/icons-material/Menu";
+import AddIcon from "@mui/icons-material/Add";
+import Box from "@mui/material/Box";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { use } from "react";
+import { useHasMounted } from "../utils/customHook";
+const AppFooter = () => {
+    const hasMounted = useHasMounted();
+    if (!hasMounted) return <></>;
+
+    return (
+        <AppBar
+            position="fixed"
+            sx={{
+                top: "auto",
+                bottom: 0,
+                backgroundColor: "#fff",
+            }}
+        >
+            <Container sx={{ display: "flex", gap: 10 }}>
+                <AudioPlayer
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+                    volume={0.5}
+
+                    // other props here
+                />
+                <div
+                    style={{
+                        display: "flex",
+                        gap: 10,
+                        flexDirection: "column",
+                        alignItems: "start",
+                        justifyContent: "center",
+                        minWidth: 100,
+                    }}
+                >
+                    <div style={{ color: "#ccc" }}>Ẻic</div>
+                    <div style={{ color: "black" }}>Who am I?</div>
+                </div>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default AppFooter;
