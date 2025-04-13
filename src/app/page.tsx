4a6069ab176836327d89e02cbae5f83d667577dd
Locvpo1233211 <trongnghia1112223333@gmail.com>
2025-04-13 18:21:38 +0700

    fetch api with wrapper ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import AppHeader from "@/components/header/app.header";
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
-export default function HomePage() {
+import { sendRequestJS } from "../components/utils/old.api";
+import { sendRequest } from "@/components/utils/api";
+export default async function HomePage() {
+    //  đổi lại gợi ý code
+    const res = await sendRequest<IBackendRes<ITrackTop[]>>({
+        url: "http://localhost:8000/api/v1/tracks/top",
+        method: "POST",
+        body: { category: "CHILL", limit: 1 },
+    });
+    console.log("res", res.data[0].title);
     return (
         <Container>
             <MainSlider />
