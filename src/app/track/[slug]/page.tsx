35a92a2e79a5749f5104885f0f8cbbb8dd58bff2
Locvpo1233211 <trongnghia1112223333@gmail.com>
2025-04-14 08:51:23 +0700

    useSearchParams

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
 const DetailTrackPage = (props: any) => {
     console.log("DetailTrackPage", props.params.slug);
+    const searchParams = useSearchParams();
+
+    const search = searchParams.get("audio");
+    console.log("searchParams", searchParams.toString());
+    console.log("search", search);
     return (
         <div className="flex flex-col gap-4">
             <h1>Detail Track Page</h1>
